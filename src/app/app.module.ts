66759bef3c8bb457bef36ffdb21d3092eaf3e0a5
AbdulMuqsitHakmi12001 <abdulmuqsithakmi12001@gmail.com>
2023-07-11 01:09:25 +0500

    Work Done upto homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavibarComponent } from './navibar/navibar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutpageComponent } from './aboutpage/aboutpage.component';
+import { InfoboxComponent } from './infobox/infobox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavibarComponent,
+    HomepageComponent,
+    AboutpageComponent,
+    InfoboxComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/infobox.ts
@@ -0,0 +1,8 @@
+export interface infoboxcontent {
+  head1: string;
+  head4: string;
+  para: string;
+  imgurl: string;
+
+  disp: string;
+}
